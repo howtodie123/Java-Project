e24570d701c03e4de2b226c229282ab4e8fb7e2c
howtodie123 <nguyenletienphat123123@gmail.com>
2025-02-22 23:09:24 +0700

    create seeder for role

---
+++ SpringbootApplication/src/main/java/com/example/springbootapplication/seeders/RoleDataSeeder.java
@@ -0,0 +1,34 @@
+package com.example.springbootapplication.seeders;
+
+import com.example.springbootapplication.entity.ERole;
+import com.example.springbootapplication.entity.Role;
+import com.example.springbootapplication.repository.RoleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class RoleDataSeeder {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @EventListener
+    @Transactional
+    public void LoadRoles(ContextRefreshedEvent event) {
+
+        List<ERole> roles = Arrays.stream(ERole.values()).toList();
+
+        for(ERole erole: roles) {
+            if (roleRepository.findByName(erole)==null) {
+                roleRepository.save(new Role(erole));
+            }
+        }
+
+    }
+}

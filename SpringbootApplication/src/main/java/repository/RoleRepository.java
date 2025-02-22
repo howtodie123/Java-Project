26a499bd6ada2b585aa8f33d0e25fcb7da70d144
howtodie123 <nguyenletienphat123123@gmail.com>
2025-02-22 22:16:19 +0700

    create Role and User Repository

---
+++ SpringbootApplication/src/main/java/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import com.example.springbootapplication.entity.Role;
+import com.example.springbootapplication.entity.ERole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(ERole name);
+}
